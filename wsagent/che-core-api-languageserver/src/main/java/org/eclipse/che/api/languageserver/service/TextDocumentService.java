6a91dafc3918c0fd0320e6b35b003961368f1f1c
Thomas Mäder <tmader@redhat.com>
2017-09-01 14:00:42 +0200

    Makes support for class file contents work
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- wsagent/che-core-api-languageserver/src/main/java/org/eclipse/che/api/languageserver/service/TextDocumentService.java
+++ wsagent/che-core-api-languageserver/src/main/java/org/eclipse/che/api/languageserver/service/TextDocumentService.java
@@ -70,6 +70,7 @@ import org.eclipse.lsp4j.SymbolInformation;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
 import org.eclipse.lsp4j.TextDocumentPositionParams;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.LanguageServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -795,13 +796,14 @@ public class TextDocumentService {
     if (server == null) {
       throw new JsonRpcException(-27000, "did not find language server");
     }
-    org.eclipse.lsp4j.services.TextDocumentService originatingService =
-        server.getServer().getTextDocumentService();
+    LanguageServer originatingService = server.getServer();
     if (!(originatingService instanceof FileContentAccess)) {
       throw new JsonRpcException(-27000, "language server does not implement file access");
     }
     try {
-      return ((FileContentAccess) originatingService).getFileContent().get(10, TimeUnit.SECONDS);
+      return ((FileContentAccess) originatingService)
+          .getFileContent(params.getUri())
+          .get(10, TimeUnit.SECONDS);
     } catch (InterruptedException | ExecutionException | TimeoutException e) {
       throw new JsonRpcException(-27000, e.getMessage());
     }
